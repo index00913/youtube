64d7d8057908927ad167dfe57cd9e599d7896c92
index00913 <jisu00913@gmail.com>
2023-09-22 13:32:50 +0900

    썸네일 추가

---
+++ main.js
@@ -0,0 +1,29 @@
+const frame = document.querySelector('section');
+const api_key = 'AIzaSyDqvoC8xc_hHGMQLtNaUT7GBsbUTB_uHyo';
+const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
+const num = 5;
+const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+fetch(resultURL)
+	.then((data) => data.json())
+	.then((json) => {
+		console.log(json.items);
+		let tags = '';
+
+		json.items.map((data) => {
+			tags += `
+    <article>
+      <h2>${data.snippet.title}</h2>
+      <div class='txt'>
+        <p>${data.snippet.description}</p>
+        <span>${data.snippet.publishedAt}</span>
+      </div>
+      <div class='pic'>
+        <img src='${data.snippet.thumbnails.standard.url}' />
+      </div>
+    </article>
+    `;
+		});
+
+		frame.innerHTML = tags;
+	});
