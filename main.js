e5da94c0b21e287ceb94400a8e8e2616d86e0b5f
index00913 <jisu00913@gmail.com>
2023-09-22 14:27:45 +0900

    특정 문자값에서 구분자를 분리한다음 이어붙이기

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const pid = 'PLHtvRFLN5v-W5bQjvyH8QTdQQhgflJ3nu';
 const num = 5;
 const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 const tit_len = 50;
+const desc_len = 180;
 
 //일정 글자수 이상일때 글자짜르고 말줄임표 붙이기
 //문자열.substr(시작위치,자를 글자수)
@@ -16,6 +17,13 @@ fetch(resultURL)
 		let tags = '';
 
 		json.items.map((data) => {
+			let desc = data.snippet.description;
+			desc.length > 20 ? (desc = desc.substr(0, 120) + '...') : desc;
+
+			//날자값 가공
+			let date = data.snippet.publishedAt.split('T')[0];
+			date = date.split('-').join('.');
+
 			tags += `
     <article>
       <h2>${
@@ -24,8 +32,8 @@ fetch(resultURL)
 					: data.snippet.title
 			}</h2>
       <div class='txt'>
-        <p>${data.snippet.description}</p>
-        <span>${data.snippet.publishedAt}</span>
+        <p>${desc}</p>
+        <span>${data.snippet.publishedAt.split('T')}</span>
       </div>
       <div class='pic'>
         <img src='${data.snippet.thumbnails.standard.url}' />
